6255027e7bad51c71b5959be3c24fdc55860b1f5
Alex S <coolvlogs29@gmail.com>
2022-11-12 10:27:05 +0200

    Added git branch

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,5 +1,9 @@
 function fish_prompt
-  # Customize fish prompt
-  printf "\n $USER @ $HOSTNAME $(pwd) \n"
+  set -l cwd (prompt_pwd)
+  set -l branch (command git symbolic-ref HEAD | sed -e 's|^refs/heads/||')
+  printf "\n $USER @ $(hostname) $cwd \n"
+  if test -n "$branch"
+      set_color blue
+      printf "  $branch "
   printf "  "
 end
